a9a0e270b78f3bbe7224f6454497f7ca1ccfd088
Ethan Mai <57975262+iAmEthanMai@users.noreply.github.com>
2023-01-14 14:47:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,13 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.write('hello world')
 st.write('this is a test')
 
+view_state_austin = pdk.ViewState(
+        latitude=30.2672,
+        longitude=-97.7431,
+        zoom=10
+    )
+r = pdk.Deck(layers=[], initial_view_state=view_state_austin, tooltip={})
+    
+st.pydeck_chart(r)
+
 
