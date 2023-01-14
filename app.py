1825a7b3b288c1dcb03b6a5954765eef37f40406
Ethan Mai <57975262+iAmEthanMai@users.noreply.github.com>
2023-01-14 14:07:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        .css-1dp5vir {visibility: hidden;}
+        .reportview-container {background: #AE8F00}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 st.write('hello world')
-
+st.write('this is a test')
 
