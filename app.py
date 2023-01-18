33c614146e9130a114793c4e53935baae3c66617
Ethan Mai <iam.ethanmai@gmail.com>
2023-01-18 12:28:16 -0500

    graph

--- app.py
+++ app.py
@@ -37,8 +37,10 @@ PIPE_COST = 10 #$/m
 
 @st.cache(suppress_st_warning=True)
 def load_graph():
-    return nx.read_gpickle('montreal_graph.pickle')
+    #return nx.read_gpickle('montreal_graph.pickle')
     #return ox.get_graph_from_place('Montreal, Quebec, Canada')
+    
+    return ox.load_graphml('simplified.graphml') 
     
 
 
